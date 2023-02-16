d17306a1ddf936147da06f5af644fcdce08e9178
botketan <ketansingh918@gmail.com>
2023-02-16 15:12:54 +0530

    Sending Live

---
+++ Models/Dish.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const Restaurant=require('./Restaurant');
+
+const dish = new mongoose.Schema({
+    Rest_Id: {
+        type: mongoose.Schema.Types.ObjectId, ref: `Restaurant`,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        enum: ['veg', 'nonveg'],
+    }, price: {
+        type: Number,
+        required: true,
+    },
+    suggestedTime: {
+        enum: ['Breakfast', 'Lunch', 'Dinner'],
+    },
+    InStock: {
+        type: Boolean,
+        required: true,
+    },
+    pic: {
+        data: Buffer,
+        contentType: String
+    }
+})
+module.exports= mongoose.model('Dish', dish);
